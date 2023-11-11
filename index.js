c7bbb92d5702beb0791fdc3f11ea87c694073141
saad200224 <saad19021994@outlook.com>
2023-11-12 02:25:12 +0600

    59-9

--- index.js
+++ index.js
@@ -72,6 +72,27 @@ async function run() {
       console.log(booking)
     })
 
+    app.patch('/bookings/:id', async (req, res) =>{
+      const id = req.params.id;
+      const filter = { _id : new ObjectId(id) }
+      const updatedBooking = req. body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const  query = { _id : new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result);
+    })
+
 
 
 
