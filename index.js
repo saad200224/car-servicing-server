1b50630ea4a90d3f2aa75a6c8d38e29d1a01b916
saad200224 <saad19021994@outlook.com>
2023-11-13 13:05:08 +0600

    60-5

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+
 app.use(express.json())
+app.use(cookieParser())
 
 // mongodb codes
 
@@ -78,6 +84,7 @@ async function run() {
 
     app.get('/bookings', async (req, res) => {
       console.log(req.query.email);
+      console.log('tok tok token', req.cookies.token)
       let query = {};
       if (req.query?.email){
         query = { email : req.query.email }
